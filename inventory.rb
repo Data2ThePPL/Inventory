6c54749e8c8f5e9af64811e814c9e9c3b1727622
Data2ThePPL <shearer.keri@gmail.com>
2015-11-01 10:52:47 -0500

    added items
    added items and prices. A list of items and prices prints to the screen.

--- inventory.rb
+++ inventory.rb
@@ -5,3 +5,11 @@
 # Allow the user to update existing items in the inventory and print the updated items to the screen
 # Include a prompt to update additional items (after printing the new results)
 # Allow the user to Add items to the inventory and print the new results to the screen
+
+sneaker_inventory = {
+    "Nike" => 120,
+    "Skechers" => 65,
+    "Converse" => 90,
+    "Rebok" => 75
+  }
+puts sneaker_inventory
