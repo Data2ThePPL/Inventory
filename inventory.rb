0efbfa953746aecb3a4305265473390f9d83b42d
Data2ThePPL <shearer.keri@gmail.com>
2015-11-01 10:31:48 -0500

    Inventory Program
    added directions

---
+++ inventory.rb
@@ -0,0 +1,7 @@
+# This is an Sneaker Inventory Program created by Renee, Keri and Paul
+
+# Objective
+# Make the inventory print out a list of items
+# Allow the user to update existing items in the inventory and print the updated items to the screen
+# Include a prompt to update additional items (after printing the new results)
+# Allow the user to Add items to the inventory and print the new results to the screen
