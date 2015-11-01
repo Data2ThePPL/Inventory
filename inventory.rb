e6f5f176c055408e0978ec75e8af5d07047f4216
Data2ThePPL <shearer.keri@gmail.com>
2015-11-01 11:31:36 -0500

    completed with hash method
    It would be cool to see if the list could be done without listing the =>

--- inventory.rb
+++ inventory.rb
@@ -6,10 +6,25 @@
 # Include a prompt to update additional items (after printing the new results)
 # Allow the user to Add items to the inventory and print the new results to the screen
 
+puts "this is the current sneaker inventory list"
+
 sneaker_inventory = {
     "Nike" => 120,
     "Skechers" => 65,
     "Converse" => 90,
     "Rebok" => 75
   }
+
+puts sneaker_inventory
+
+# user adds to the list
+
+puts "type a new sneaker name and hit enter to add item to the list"
+
+sneaker_inventory[gets.chomp] = 0
+
+puts "type the price of the new sneaker"
+
+sneaker_inventory [sneaker_inventory.keys.last] = gets.chomp.to_i #fromhttp://stackoverflow.com/questions/7749131/accessing-the-last-key-value-pair-in-a-ruby-1-9-hash
+
 puts sneaker_inventory
